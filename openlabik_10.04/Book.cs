0fb6754d3709c58191d3a056028711978dc8cbac
Matusko <matusnekoranec22@gmail.com>
2022-12-05 20:28:11 +0100

    Add project files.

---
+++ openlabik_10.04/Book.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openlabik_10._04
+{
+    internal class Book
+    {
+        private string title;
+        private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
+
+        public Book()
+        {
+            title = "-1";
+            pages = -1;
+            category = "-1";
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string sTitle, int iPages)
+        {
+            this.title = sTitle;
+            this.pages = iPages;
+            category = "-1";
+            category = "-1";
+            author = "-1";
+            releaseDate = -1;
+        }
+
+        public Book(string sTitle, int iPages, string sCategory, string sAuthor, int iReleaseDate)
+        {
+            this.title = sTitle;
+            this.pages = iPages;
+            this.category = sCategory;
+            this.author = sAuthor;
+            this.releaseDate = iReleaseDate;
+        }
+        public string Title { get { return title; } set { title = value; } }
+
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                pages = pages = value <= 0 ? 1 : value;
+            }
+        }
+        public string Category { get { return category; } set { category = value; } }
+        public string Author { get { return author; } set { author = value; } }
+
+        public int ReleaseDate
+        {
+            get
+            {
+                return releaseDate;
+
+            }
+            set
+            {
+                releaseDate = releaseDate = value <= 1450 || value >= 2021 ? -1 : value;
+            }
+        }
+        public override string ToString()
+        {
+            return String.Format($"\n{title}\n{pages}\n{category}\n{author}\n{releaseDate}\n");
+        }
+    }
+}
---
+++ openlabik_10.04/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace openlabik_10._04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   Book LOTR = new Book();
+              LOTR.Title = "Lord Of The Rings";
+               LOTR.Pages = 576;
+               LOTR.Category = "Fantasy";
+               LOTR.Author = "J.R.R.Tolkien";
+               LOTR.ReleaseDate = 1954;
+               Console.WriteLine(LOTR.ToString());*/
+
+            Book LOTR2 = new Book();
+            Console.WriteLine(LOTR2.ToString());
+            Console.WriteLine();
+
+            Book LOTR3 = new Book("Lord Of The Rings 3", 1536);
+            Console.WriteLine(LOTR3.ToString());
+
+            Book HOBIT = new Book("Hobit", 304, "Fantasy", "J. R. R. Tolkien", 1937);
+            Console.WriteLine(HOBIT.ToString());
+        }
+    }
+}
